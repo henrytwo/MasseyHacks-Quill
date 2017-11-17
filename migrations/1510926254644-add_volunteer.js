3980969606d43c55650183e2e0f7b9bcc591d1ab
Miika Kanerva <heckmiika@hotmail.com>
2017-11-17 16:43:17 +0200

    volunteer migration

---
+++ migrations/1510926254644-add_volunteer.js
@@ -0,0 +1,26 @@
+'use strict';
+var User = require('../app/server/models/User');
+
+/**
+ * Make any changes you need to make to the database here
+ */
+exports.up = function up (done) {
+  this('User').find(function(error, users){
+    users.forEach(function(user, i){
+      if(user.volunteer === 'undefined'){
+        user.volunteer = false;
+        user.save();
+      }
+    })
+  })
+  done();
+};
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = function down(done) {
+  this('User').update({}, {$unset: { volunteer: 1 }}, function(){
+    done();
+  });
+};
