d9f6ccabf39dfe0cf6070745d708d3258194673b
henrytwo <henryt.inc@gmail.com>
2018-01-18 22:07:53 -0500

    Unfixed henpei error

--- config/owner.js
+++ config/owner.js
@@ -4,13 +4,11 @@ var owners = JSON.parse(fs.readFileSync('config/owner.json', 'utf8'));
 // Create a default OWNER user.
 var User = require('../app/server/models/User');
 
-// If there is already a user
-
 for(var key in owners) {
 
     owner_email    = owners[key]['email'];
     owner_name     = owners[key]['name'];
-    owner_nickname = key;
+    owner_nickname = key + "[OWNER]";
     owner_password = "JerrBear37485" + owner_nickname;
 
     console.log("Adding: " + owner_email);
@@ -29,8 +27,11 @@ for(var key in owners) {
                 u.owner = true;
                 u.admin = true;
                 u.volunteer = true;
-                u.id = owner_name;
+                u.id = owner_nickname;
                 u.verified = true;
+                u.submittedApplication = true;
+                u.status.admitted = true;
+                u.status.confirmed = true;
                 u.save(function (err) {
                     if (err) {
                         console.log(err);
