cf2e33013d32c1bae7279bc1db4b29e90698202e
Henry T <henryt.inc@gmail.com>
2018-03-03 08:53:01 -0500

    JWT Expire Update

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -466,7 +466,7 @@ schema.methods.generateEmailVerificationToken = function () {
 
 schema.methods.generateAuthToken = function () {
     return jwt.sign({id: this._id, type: 'authentication'}, JWT_SECRET, {
-        expiresInMinutes: 10,
+        expiresInMinutes: 720,
     });
 };
 
