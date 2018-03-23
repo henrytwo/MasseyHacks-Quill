98746b1b89647c372a44b1920df4039b52c77813
Henry T <henryt.inc@gmail.com>
2018-03-23 18:12:57 -0400

    Slack error fix

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -407,7 +407,7 @@ module.exports = function(router) {
    * GET - Get a specific user.
    */
   router.get('/slack/:id', isOwnerOrAdmin, function(req, res){
-      UserController.inviteToSlack(req.params.id, res.send(req));
+      UserController.inviteToSlack(req.params.id, res.json(data));
   });
 
 
