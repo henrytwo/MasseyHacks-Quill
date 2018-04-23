e9008632a50caa2db04d9a768b38ed8d85693d5e
Ryan Zhang <ryanzhang0112@gmail.com>
2018-04-23 18:22:10 -0400

    small QR update (Switched to a fork)

--- app/client/views/checkin/checkinCtrl.js
+++ app/client/views/checkin/checkinCtrl.js
@@ -34,7 +34,7 @@ angular.module('reg')
         '$state',
         function ($scope, $stateParams, UserService, $state) {
 
-            loadScript("https://rawgit.com/schmich/instascan-builds/master/instascan.min.js", function () {
+            loadScript("https://raw.githubusercontent.com/chaelli/instascan/master/dist/instascan.min.js", function () {
                 scanner = new Instascan.Scanner({video: document.getElementById('preview'), mirror:false});
 
                 scanner.addListener('scan', function (data, image) {
